985122f33936e79230cd22e8706f0adcdadc433a
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2021-12-09 09:55:51 +0100

    Add gitignore

--- API/app.js
+++ API/app.js
@@ -1,1 +1,19 @@
-// App for API
+const express = require('express');
+
+const DataBase = require("./database/database");
+
+const userRoute = require("./routes/userRoute");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+DataBase.connect();
+
+app.use('/users', userRoute);
+
+
+app.listen(port, () => console.log(`http://localhost:${port}/`));
+

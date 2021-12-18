d75eab82414135d4677d6e878672b973c8a904bf
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2021-12-18 18:42:05 +0100

    Sever

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,13 @@
-// App for Server
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3050;
+
+app.use(express.static(__dirname + '/Front'));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'Front', 'mainpage.html'));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
